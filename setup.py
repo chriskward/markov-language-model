c78464f433cce48b62f73cd3b6e28c779a2481fa
Chris Ward <c.ward.uk@gmail.com>
2023-09-10 20:28:47 +0100

    setup.py and __init__.py + package folder structure

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup( name = "markovmodel",
+	version = "1.02",
+	author = 'Chris Ward',
+	author_email = 'chrisward@email.com',
+	packages=find_packages(include=['languagemodel','languagemodel.*']),
+	install_requires = ['numpy>=1.26.0','pandas'])
