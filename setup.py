77b2927bb8806e3adc148411d9abfd43d1c8d926
Chris Ward <c.ward.uk@gmail.com>
2023-09-10 21:40:02 +0100

    Bug fixes ....

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ setup( name = "markovmodel",
 	version = "1.02",
 	author = 'Chris Ward',
 	author_email = 'chrisward@email.com',
-	packages=find_packages(include=['languagemodel','languagemodel.*']),
+	packages=find_packages(include=['markovmodel','markovmodel.*']),
 	install_requires = ['numpy>=1.26.0','pandas'])
